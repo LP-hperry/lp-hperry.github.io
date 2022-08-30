d3349027e32d8da9ee43d7d7ee37a06837716cf0
LP-hperry <94328767+LP-hperry@users.noreply.github.com>
2022-08-30 11:28:50 +1000

    Create app.js

---
+++ app.js
@@ -0,0 +1,44 @@
+(function (w, d, ns) {
+
+    var sendBtn = d.getElementById('sendBtn');
+    var textRadioChosen = d.getElementById('text');
+    var jsonRadioChosen = d.getElementById('json');
+    var selectContentBtn = d.getElementById('selectContentBtn');
+    var contentOptions = d.getElementById('contentOptions');
+    var selectQuickRepliesBtn = d.getElementById('selectQuickRepliesBtn');
+    var quickRepliesOptions = d.getElementById('quickRepliesOptions');
+    var clearLogBtn = d.getElementById('clearLogBtn');
+    var increaseFontSizeBtn = d.getElementById('increaseFontSizeBtn');
+    var decreaseFontSizeBtn = d.getElementById('decreaseFontSizeBtn');
+
+    function bindCommand(el, cmd) {
+        el.addEventListener('click', ns.commands[cmd]);
+    }
+
+    function initSdk() {
+        lpTag.agentSDK.init({});
+    }
+
+    function init() {
+
+        ns.commands.populateContentListByType('text');
+        ns.commands.populateQuickReplies();
+
+        bindCommand(sendBtn, 'send');
+        bindCommand(textRadioChosen, 'changeContentType');
+        bindCommand(jsonRadioChosen, 'changeContentType');
+        bindCommand(selectContentBtn, 'toggleContentOptionsList');
+        bindCommand(contentOptions, 'addContent');
+        bindCommand(selectQuickRepliesBtn, 'toggleQuickRepliesList');
+        bindCommand(quickRepliesOptions, 'addQuickReplies');
+        bindCommand(clearLogBtn, 'clearLog');
+        bindCommand(increaseFontSizeBtn, 'increaseFontSize');
+        bindCommand(decreaseFontSizeBtn, 'decreaseFontSize');
+
+        initSdk();
+    }
+
+
+    init();
+
+})(window, document, window.quickRepliesWidget = window.quickRepliesWidget || {});
